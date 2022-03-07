be1d91294c7767cac13943ce8b353e3d03bfce5b
cnsodom <cnsodom@gmail.com>
2022-03-07 07:30:19 -0600

    tab conversion to spaces

--- asg_01_cylinder_weight/scripts/asg_01_weight_calc.py
+++ asg_01_cylinder_weight/scripts/asg_01_weight_calc.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg 		import Float64
-from asg_01_cylinder_weight.msg 	import Cylinder
+from std_msgs.msg       import Float64
+from asg_01_cylinder_weight.msg     import Cylinder
 
-volume 			= 0
-density			= 0
+volume          = 0
+density         = 0
 
 density_found   = False
-volume_found	= False
+volume_found    = False
 
 def density_callback(data):
-	global density
-	global density_found
-	density			= data.data
-	density_found 	= True
+    global density
+    global density_found
+    density         = data.data
+    density_found   = True
 
 def cylinder_callback(data):
-	global volume
-	global volume_found
-	volume 			= data.volume
-	volume_found 	= True
+    global volume
+    global volume_found
+    volume          = data.volume
+    volume_found    = True
 
 def calculate():
-	if density_found and volume_found:
-		weight = 9.807 * density * volume
-		density_pub.publish(weight)
+    if density_found and volume_found:
+        weight = 9.807 * density * volume
+        density_pub.publish(weight)
 
 rospy.init_node("weight_calc")
 rospy.Subscriber("/density",Float64,density_callback)
@@ -33,5 +33,5 @@ rospy.Subscriber("/cylinder",Cylinder,cylinder_callback)
 density_pub = rospy.Publisher("/weight",Float64,queue_size=10)
 
 while not rospy.is_shutdown():
-	calculate()
-	rospy.sleep(0.1)
+    calculate()
+    rospy.sleep(0.1)
--- asg_01_cylinder_weight/scripts/radius_squarer.py
+++ asg_01_cylinder_weight/scripts/radius_squarer.py
@@ -3,9 +3,9 @@ import rospy
 from std_msgs.msg import Float64
 
 def callback(data):
-	radius 			= data.data
-	squared_radius 	= radius * radius
-	pub.publish(squared_radius)
+    radius          = data.data
+    squared_radius  = radius * radius
+    pub.publish(squared_radius)
 
 rospy.init_node("radius_squarer")
 pub = rospy.Publisher("/radius_squared",Float64,queue_size=10)
